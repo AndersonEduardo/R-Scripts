70f98b6db1198d8d0863b9980e2e20d7c63f9954
AndersonEduardo <andersonaed@gmail.com>
2017-05-18 16:54:55 -0300

    Primeira versao finalizada para teste tranferibilidade (com o framework do paper)

--- artificialSps.R
+++ artificialSps.R
@@ -642,6 +642,7 @@ dev.off()
 
 
 ###SETIMA PARTE: transferibilidade pelo framework de xxx(2016?)###
+
 library(dismo)
 library(phyloclim)
 library(virtualspecies)
@@ -655,7 +656,6 @@ mainSampleFolder = '/home/anderson/Documentos/Projetos/Sps artificiais/Amostras/
 maxentFolder = '/home/anderson/Documentos/Projetos/Sps artificiais/Maxent/' #pasta para resultados do maxent
 spsTypes = c('spHW', 'spHD', 'spCD') #nomes das especies
 source("/home/anderson/R/R-Scripts/TSSmaxent.R")
-
 
 
 ##ocorrencias
@@ -682,7 +682,7 @@ for (i in 1:length(spsTypes)){ #loop sobre os 'tipos de especies'
     }
 }
 
-##testando os modelos
+##TESTANDO MODELOS: calibracao 'monotemporal'
 
 spOccFolder = '/home/anderson/Documentos/Projetos/Sps artificiais/Amostras/transferability/'
 ##abrindo um data.frame para armazenar os resultados de AUC
@@ -694,7 +694,6 @@ TSSvector = data.frame()
 sampleSizes = c(5,15,25,35,45,55,65,75,85,95)
 NumRep = 3 #numero de replicas (de cada cenario amostral)
 sampledAges = c(0,21)
-projection(auxVector[[j]]) = CRS('+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0') #ajustando CRS
 predictors0k = stack(list.files(path=envVarPaths[1],full.names=TRUE, pattern='.asc')) #predictors com todas as variaveis (presente)
 projection(predictors0k)=CRS('+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0')
 predictors21k = stack(list.files(path=envVarPaths[22],full.names=TRUE, pattern='.asc')) #predictors com todas as variaveis (22kyr BP)
@@ -806,6 +805,143 @@ for (i in 1:length(spsTypes)){
     }
 }
 
-write.csv(aucOutput,paste(spOccFolder,'/aucOutputs.csv',sep=''),row.names = FALSE)
+write.csv(aucOutput,paste(spOccFolder,'/aucOutputsMonotemp.csv',sep=''),row.names = FALSE)
 
+
+##TESTANDO MODELOS: calibracao 'multitemporal'
+
+
+for (i in 1:length(spsTypes)){
+    for (j in sampledAges){
+        for (k in sampleSizes){
+            for (l in 1:NumRep){
+    
+                sp.file.intern <- read.csv(paste(spOccFolder,spsTypes[i],'/',j,'k',k,'pts',l,"rep.csv",sep=""),header=TRUE) ### read sp occurrence
+                sp.occ.intern <- sp.file.intern[,c('lon','lat')] ## select lat-long columns
+
+                sp.file.extern <- read.csv(paste(spOccFolder,spsTypes[i],'/',sampledAges[which(sampledAges!=j)],'k',k,'pts',l,"rep.csv",sep=""),header=TRUE) ### read sp occurrence
+                sp.occ.extern <- sp.file.extern[,c('lon','lat')] ## select lat-long columns
+                
+                ##extraindo dados da variavel climatica nos pontos de ocorrencia
+                presencesVarsIntern <- sp.file.intern[,c('bioclim_01','bioclim_12')]
+                presencesVarsExtern <- sp.file.extern[,c('bioclim_01','bioclim_12')]
+    
+                ##criando um vetor de presenca para usar em uma coluna de presenca/ausencia na tabela final
+                pres = rep(1, nrow(presencesVarsIntern))
+    
+                ##juntando dados das variaveis climaticas nos pontos de ocorrencia, coordenadas de ocorrencia e o vetor (coluna na tabela) para presenca/ausencia
+                presencesDataIntern = data.frame(cbind(sp.occ.intern,pres,presencesVarsIntern))
+                presencesDataIntern = presencesDataIntern[complete.cases(presencesDataIntern),]
+                presencesDataIntern = presencesDataIntern[complete.cases(presencesDataIntern),]
+                ##
+                presencesDataExtern = data.frame(cbind(sp.occ.intern,pres,presencesVarsExtern))
+                presencesDataExtern = presencesDataExtern[complete.cases(presencesDataExtern),]
+                presencesDataExtern = presencesDataExtern[complete.cases(presencesDataExtern),]
+    
+                ##criando pontos de background
+                backgroundIntern1 <- randomPoints(mask=predictors[[which(sampledAges==j)]][[1]], n=5000, p=presencesDataIntern[,c("lon","lat")], excludep=TRUE)
+                backgroundIntern2 <- round(background1, digits=2)
+                backgroundIntern3 <- backgroundIntern2[!duplicated(backgroundIntern2),]
+                backgroundIntern4 <- backgroundIntern3[complete.cases(backgroundIntern3),]
+                backgroundIntern <- data.frame(backgroundIntern4)
+                colnames(backgroundIntern) <- c("lon", "lat")
+                ##
+                backgroundExtern1 <- randomPoints(mask=predictors[[which(sampledAges!=j)]][[1]], n=5000, p=presencesDataExtern[,c("lon","lat")], excludep=TRUE)
+                backgroundExtern2 <- round(background1, digits=2)
+                backgroundExtern3 <- backgroundExtern2[!duplicated(backgroundExtern2),]
+                backgroundExtern4 <- backgroundExtern3[complete.cases(backgroundExtern3),]
+                backgroundExtern <- data.frame(backgroundExtern4)
+                colnames(backgroundExtern) <- c("lon", "lat")
+                
+                ##extraindo dados da variavel climatica nos pontos de background
+                
+                backgroundVarsIntern <- extract(predictors[[which(sampledAges==j)]], backgroundIntern1, method='bilinear', buffer=NULL, fun=NULL)
+                backgroundVarsExtern <- extract(predictors[[which(sampledAges!=j)]], backgroundExtern1, method='bilinear', buffer=NULL, fun=NULL)
+                
+
+                ##criando um vetor de ausencias para usar em uma coluna de presenca/ausencia na tabela final
+                pres = rep(0, nrow(backgroundVarsIntern))
+    
+                ##juntando dados das variaveis climaticas nos pontos de ocorrencia, coordenadas de ocorrencia e o vetor (coluna na tabela) para presenca/ausencia    
+                backgroundDataIntern = data.frame(cbind(backgroundIntern,pres,backgroundVarsIntern[,c('bioclim_01','bioclim_12')]))
+                backgroundDataIntern = backgroundDataIntern[complete.cases(backgroundDataIntern),]
+                ##
+                backgroundDataExtern = data.frame(cbind(backgroundExtern,pres,backgroundVarsExtern[,c('bioclim_01','bioclim_12')]))
+                backgroundDataExtern = backgroundDataExtern[complete.cases(backgroundDataExtern),]    
+    
+                ##planilha de dados final
+                dataSet = list()
+                for (m in 1:10){
+                    rand = sample(c(rep(1,round(nrow(presencesDataIntern)*0.5)),rep(0,nrow(presencesDataIntern)-round(nrow(presencesDataIntern)*0.5))))
+                    presencesTrain = rbind(presencesDataIntern[rand==0,],presencesDataExtern[rand==1,])
+                    backgroundTrain = rbind(backgroundDataIntern[rand==0,],backgroundDataExtern[rand==1,])
+                    dataSet_i = data.frame(c(rbind(presencesTrain,backgroundTrain)))
+                    dataSet[[m]] = dataSet_i
+                }
+
+                ##avaliando o modelo
+                evaluation = list()
+    
+                for (n in 1:10){
+                    tryCatch({# bootstrapping with 10 replications
+            
+                        ##reparando uma porcao dos dados de presenca e ausencia (background) para calibrar (treinar) o modelo
+                        ##rand = round(0.75*runif(nrow(dataSet[[n]][dataSet[[n]]$pres==1,])))
+                        rand = sample(c(rep(1,round(nrow(dataSet[[n]][dataSet[[n]]$pres==1,])*0.25)),rep(0,nrow(dataSet[[n]][dataSet[[n]]$pres==1,])-round(nrow(dataSet[[n]][dataSet[[n]]$pres==1,])*0.25))))
+                        presencesTrain = dataSet[[n]][dataSet[[n]]$pres==1,][rand==0,]
+                        backgroundTrain = dataSet[[n]][dataSet[[n]]$pres==0,][rand==0,]
+            
+                        ##juntando presencas e ausencias da calibracao
+                        PresBackTrainRaw <- data.frame(rbind(presencesTrain, backgroundTrain))
+                        PresBackTrainRaw = PresBackTrainRaw[!duplicated(PresBackTrainRaw[,c('lon','lat')]),] #selecionar colunas de longitude e latitude
+                        PresBackTrainRaw <- PresBackTrainRaw[complete.cases(PresBackTrainRaw),]
+                        PresBackTrain = PresBackTrainRaw
+
+                        ##CRIANDO E RODANDO O MODELO (esquema do SWD - sample with data)##    
+                        MX <- maxent(x=PresBackTrain[,c('bioclim_01','bioclim_12')],p=PresBackTrain$pres,args=c(
+                                                                                           'responsecurves=true',
+                                                                                           'jackknife=true',
+                                                                                           'randomseed=true',
+                                                                                           'randomtestpoints=0',
+                                                                                           'replicates=1',
+                                                                                           'writebackgroundpredictions=true',
+                                                                                           'linear=true',
+                                                                                           'quadratic=true',
+                                                                                           'product=false',
+                                                                                           'threshold=false',
+                                                                                           'hinge=false',
+                                                                                           'maximumiterations=1000',
+                                                                                           'convergencethreshold=1.0E-5',
+                                                                                           'threads=2'))
+                        
+                        ##avaliacao INTERNA
+                        presencesTestIntern = presencesDataIntern[rand==1,]
+                        presencesTestIntern <- presencesTestIntern[,c('lon','lat')]
+                        backgroundTestIntern = backgroundDataIntern[rand==1,]
+                        backgroundTestIntern = backgroundTestIntern[,c('lon','lat')]
+                        
+                        ##rodando a avaliacao do modelo
+                        internalEval = evaluate(p=presencesTest,a=backgroundTest,m=MX,x=predictors[[which(sampledAges==j)]],type='response')
+
+                        ##avaliacao EXTERNA
+                        presencesTestExtern = presencesDataExtern[rand==1,]
+                        presencesTestExtern <- presencesTestExtern[,c('lon','lat')]
+                        backgroundTestExtern = backgroundDataExtern[rand==1,]
+                        backgroundTestExtern = backgroundTestExtern[,c('lon','lat')]
+
+                        ##rodando a avaliacao do modelo
+                        externalEval = evaluate(p=presencesTestExtern,a=backgroundTestExtern,m=MX,x=predictors[[which(sampledAges!=j)]],type='response')
+
+                        ##salvando AUC interno e externo
+                        aucOutputMultitemp = rbind(aucOutputMultitemp,data.frame(sp=spsTypes[i], sample_size=k, replicate=l, AUC_int=internalEval@auc, AUC_ext=externalEval@auc,int_age=j,ext_age=sampledAges[which(sampledAges!=j)]))
+                        
+                        
+                    }, error=function(e){cat("ERROR :",conditionMessage(e),"\n")}
+                    )}
+            }
+        }
+    }
+}
+
+write.csv(aucOutput,paste(spOccFolder,'/aucOutputsMultitemp.csv',sep=''),row.names = FALSE)
 
