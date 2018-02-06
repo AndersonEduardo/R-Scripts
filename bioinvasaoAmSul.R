c17f2e21b42267a6bac4ed07da3b5486fa5a6d69
AndersonEduardo <andersonaed@gmail.com>
2018-02-06 17:52:22 -0300

    primeiros progressos

--- bioinvasaoAmSul.R
+++ bioinvasaoAmSul.R
@@ -1,73 +1,66 @@
-## invasao mmuferos america do sul
+## invasao mamiferos america do sul
 
+## pacotes necessarios
 library(biomod2)
 library(raster)
 
+## parametros e variaveis globais
+projectFolder = '/home/anderson/Área de Trabalho/bioinvasao_AmSul'
+sampleFolder = '/home/anderson/Área de Trabalho/bioinvasao_AmSul/occ_data'
+envVarFolder = '/home/anderson/Área de Trabalho/bioinvasao_AmSul/env_data'
+maxentFolder = '/home/anderson/R/x86_64-pc-linux-gnu-library/3.3/dismo/java/maxent.jar' #pasta para resultados do maxent'
+
+## variaveis ambientais
 globalVars = stack(list.files('/home/anderson/PosDoc/dados_ambientais/bio_2-5m_bil', pattern='.bil', full.names=TRUE))
 
 plot(globalVars[[1]])
+##
 extEuro = drawExtent()
+extEuro = c(-9.121301, 145.8906, 6.600978, 75.33003)
+##
 extAm = drawExtent()
+extAm = c(-175.3659, -20.35405, 14.55908, 82.56467)
+##
 extAmSul = drawExtent()
+extAmSul = c(-88.49937,-28.59139,-57.7873,13.83562)
 
-euroVars = crop(globalVars[[c('bio1','bio5','bio6')]], extEuro)
-amerVars = crop(globalVars[[c('bio1','bio5','bio6')]], extAm)
-amerSulVars = crop(globalVars[[c('bio1','bio5','bio6')]], extAmSul)
+euroVars = crop(globalVars, extEuro)
+amerVars = crop(globalVars, extAm)
+amerSulVars = crop(globalVars, extAmSul)
 
+## testando correcaloes
+## eurasia
+testEurasia = getValues(euroVars)
+cor.matrix <- as.data.frame(cor(testEurasia, use="complete.obs"))
+write.csv(cor.matrix,paste(envVarFolder,'/cor_Eurasia.csv',sep=''), row.names=TRUE)
+## america do norte
+testAmNorte = getValues(amerVars)
+cor.matrix <- as.data.frame(cor(testAmNorte, use="complete.obs"))
+write.csv(cor.matrix,paste(envVarFolder,'/cor_AmNorte.csv',sep=''), row.names=TRUE)
 
+## identificando variaveis nao correlacionadas
+## eurasia
+corMatrix = read.csv(paste(envVarFolder,'/cor_Eurasia.csv',sep=''),header=TRUE, row.names=1)
+corMatrixTri = corMatrix
+corMatrixTri[upper.tri(corMatrixTri)] = 0.0
+diag(corMatrixTri) = 0.0
+corMatrixClean = corMatrix[,!apply(corMatrixTri,2,function(x) any(abs(x) > 0.7))]
+sort(names(corMatrixClean))
+## america norte
+corMatrix = read.csv(paste(envVarFolder,'/cor_AmNorte.csv',sep=''),header=TRUE, row.names=1)
+corMatrixTri = corMatrix
+corMatrixTri[upper.tri(corMatrixTri)] = 0.0
+diag(corMatrixTri) = 0.0
+corMatrixClean = corMatrix[,!apply(corMatrixTri,2,function(x) any(abs(x) > 0.7))]
+sort(names(corMatrixClean))
 
-sampleFolder = '/home/anderson/Área de Trabalho/sps_invasao'
+## construcao das variaveis preditoras
 
-spsVector = list.files(sampleFolder, pattern='nativo', full.names=TRUE )
+euroVars = euroVars[[c("bio7", "bio8", "bio9", "bio18", "bio19" )]]
+amerVars = amerVars[[c("bio7", "bio8", "bio9", "bio18", "bio19" )]]
+amerSulVars = amerSulVars[[c("bio7", "bio8", "bio9", "bio18", "bio19" )]]
 
-nativoData = read.csv(spsVector[3], header=TRUE)
-names(nativoData) = c('sp','lon','lat')
-
-
-
-##variaveis e parametros locais especificos para o biomod2
-
-occData = nativoData[,c('lon','lat')]
-##
-spName = gsub('.csv','',list.files(sampleFolder, pattern='nativo')[2])
-spName =  gsub(' ','_',spName)
-myRespName <- spName
-##
-myResp <- occData # variavel resposta (para biomod2)
-coordinates(myResp) <- ~ lon + lat #transformando em spatialPoints
-crs(myResp) = CRS('+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0') #transformando em spatialPoints
-##
-myExpl = stack(euroVars)
-
-
-##ajuste de dados de entrada para biomod2
-myBiomodData <- BIOMOD_FormatingData(resp.var = myResp,
-                                     expl.var = myExpl,
-                                     resp.name = myRespName,
-                                     PA.nb.rep = 1)
-      
-##rodando o(s) algoritmo(s) (i.e. SDMs)
-myBiomodModelOut <- BIOMOD_Modeling(
-    data = myBiomodData,
-    models = c('MAXENT.Phillips'),
-    models.options = myBiomodOption,
-    NbRunEval = 100,
-    DataSplit = 75,
-    models.eval.meth = c('TSS','ROC'),
-    do.full.models = FALSE,
-    modeling.id = myRespName)
-
-##My output data
-evaluationScores = get_evaluations(myBiomodModelOut)
-
-##rodando algortmo de projecao (i.e. rodando a projecao)
-myBiomodProj <- BIOMOD_Projection(
-    modeling.output = myBiomodModelOut,
-    new.env = myExpl_invadido,
-    proj.name = myRespName,
-    compress = 'TRUE',
-    build.clamping.mask = 'TRUE')
-
+writeRaster(x=euroVars, filename=paste(envVarFolder,euroVars,'.grd',sep=''))
 
 #######################################################################
 ############# densidade da variavel temporetatura #####################
@@ -75,14 +68,12 @@ myBiomodProj <- BIOMOD_Projection(
 #######################################################################
 
 
-##caminhos dos arquivos de dados
-spsVector = list.files(sampleFolder, full.names=TRUE )
-
 ##vetor com o nome dos arquivos
-spsNames = list.files(sampleFolder)
+spsNames = list.files(paste(sampleFolder,sep=''), full.names=FALSE )
 vecNames = gsub(pattern='_invadido.csv',replacement='',x=spsNames)
 vecNames = gsub(pattern='_nativo.csv',replacement='',x=vecNames)
 vecNames = unique(vecNames)
+vecNames = vecNames[which(vecNames != "Mus_musculus")] #retirando Mus musculus
 
 
 ##temperaturas criticas 
@@ -97,10 +88,10 @@ termNeut = data.frame(sps=c("Sus_scrofa",
 ## rattus rattus = 26 e 34
 ## castor = 0 e 28
 
-
+## comparacao grafica entre temperatura media nos dados para area nativa, area invadida e TNZ
 for(sp_i in vecNames){
     ##vetor de nomes de arquivo
-    spsVector = list.files(sampleFolder, pattern=sp_i, full.names=TRUE)
+    spsVector = list.files(paste(sampleFolder,sep=''), pattern=sp_i, full.names=TRUE)
     ##area nativa
     nativoData = read.csv(spsVector[grep(pattern='nativo',x=spsVector)], header=TRUE)
     names(nativoData) = c('sp','lon','lat')
@@ -118,9 +109,98 @@ for(sp_i in vecNames){
     tempNativa = tempNativa[complete.cases(tempNativa),] #limpando
     tempInvadido = tempInvadido[complete.cases(tempInvadido),] #limpando
     ##graficos
-    jpeg(paste('/home/anderson/Área de Trabalho/sps_invasao/',sp_i,'.jpeg', sep=''), main=)
-    plot(density(tempNativa[,'bio1']), xlim=c(-200,500), lwd=2, col='blue')
-    lines(density(tempInvadido[,'bio1']), lwd=2, col='red')
-    abline(v=termNeut[termNeut$sps==sp_i,][,c('min','max')])
+    jpeg(paste(projectFolder,'/',sp_i,'.jpeg', sep=''))
+    plot(density(tempNativa[,'bio1']),
+         ylim=c(0, max(max(density(tempNativa[,'bio1'])$y),max(density(tempInvadido[,'bio1'])$y))),
+         xlim=c(min(c(range(density(tempNativa[,'bio1'])$x), range(density(tempInvadido[,'bio1'])$x), as.numeric(termNeut[termNeut$sps==sp_i,][,c('min','max')]))),
+                max(c(range(density(tempNativa[,'bio1'])$x), range(density(tempInvadido[,'bio1'])$x), as.numeric(termNeut[termNeut$sps==sp_i,][,c('min','max')])))),
+         lwd=2,
+         col='blue',
+         xlab='Mean annual temperature',
+         main=gsub('_',' ',sp_i))
+    lines(density(tempInvadido[,'bio1']),
+          lwd=2,
+          col='red')
+    abline(v=termNeut[termNeut$sps==sp_i,][,c('min','max')], lty=2, lwd=1.5)
+    legend(x='topleft', legend=c('Native area', 'Invaded area', 'Thermal neutral zone'), lty=c(1,1,2), lwd=2, col=c('blue','red','black'), cex=0.8, bg='white')
     dev.off()
 }
+
+
+######### modelagem de nicho #########
+############# BIOMOD2 ################
+
+##vetor com o nome dos arquivos
+spsNames = list.files(paste(sampleFolder,sep=''), full.names=FALSE )
+vecNames = gsub(pattern='_invadido.csv',replacement='',x=spsNames)
+vecNames = gsub(pattern='_nativo.csv',replacement='',x=vecNames)
+vecNames = unique(vecNames)
+vecNames = vecNames[which(vecNames != "Mus_musculus")] #retirando Mus musculus
+
+for(sp_i in vecNames){
+    
+    ##vetor de nomes de arquivo
+    spsFiles = list.files(paste(sampleFolder,sep=''), pattern=sp_i, full.names=TRUE)
+    ##area nativa
+    nativoData = read.csv(spsVector[grep(pattern='nativo',x=spsVector)], header=TRUE)
+    names(nativoData) = c('sp','lon','lat')
+    ##area invadida
+
+    ##extraindo variaveis ambientais
+    if(sp_i == "Castor_canadensis"){
+        nativVars = amerVars
+    }else{
+        nativVars = euroVars}
+    ##    
+
+    ##variaveis e parametros locais especificos para o biomod2
+
+    myRespName <- sp_i
+    myResp <- nativoData[,c('lon','lat')] # variavel resposta (para biomod2)
+    coordinates(myResp) <- ~ lon + lat #transformando em spatialPoints
+    crs(myResp) = CRS('+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0') #transformando em spatialPoints
+    myExpl = stack(euroVars)
+
+    ##ajuste de dados de entrada para biomod2
+    myBiomodData <- BIOMOD_FormatingData(resp.var = myResp,
+                                         expl.var = myExpl,
+                                         resp.name = myRespName,
+                                         PA.nb.rep = 1)
+
+          ##parametrizando os modelos
+      myBiomodOption <- BIOMOD_ModelingOptions(
+        MAXENT.Phillips = list(path_to_maxent.jar= maxentFolder,
+                               linear=TRUE,
+                               quadratic=TRUE,
+                               product=FALSE,
+                               threshold=FALSE,
+                               hinge=FALSE,
+                               maximumiterations=1000,
+                               convergencethreshold=1.0E-5,
+                               threads=2))
+    
+    ##rodando o(s) algoritmo(s) (i.e. SDMs)
+    myBiomodModelOut <- BIOMOD_Modeling(
+        data = myBiomodData,
+        models = c('MAXENT.Phillips'),
+        models.options = myBiomodOption,
+        NbRunEval = 100,
+        DataSplit = 75,
+        models.eval.meth = c('TSS','ROC'),
+        do.full.models = FALSE,
+        modeling.id = paste(myRespName,'_area_nativa',sep=''))
+
+    ##My output data
+    evaluationScores = get_evaluations(myBiomodModelOut)
+
+    ##rodando algortmo de projecao (i.e. rodando a projecao)
+    myBiomodProj <- BIOMOD_Projection(
+        modeling.output = myBiomodModelOut,
+        new.env = myExpl_invadido,
+        proj.name = paste(myRespName,'_area_invadida',sep=''),
+        compress = 'TRUE',
+        build.clamping.mask = 'TRUE')
+
+}
+
+
