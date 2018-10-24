c48928aedb00284ab401dd13d0cda93f5301670a
Anderson Eduardo <andersonaed@gmail.com>
2018-10-24 16:27:35 -0300

    primeira versão final/oficial com AUC e TSS

--- SDMmegafauna.R
+++ SDMmegafauna.R
@@ -1,27 +1,45 @@
-
 ##abrindo pacotes e funcoes necessarias##
 library(raster)
 library(hypervolume)
-source('/home/anderson/R-Scripts/paleoextract.R')
-source('/home/anderson/R-Scripts/strings2na.R')
-source('/home/anderson/R-Scripts/dataInstance.R')
-source('/home/anderson/R-Scripts/uplot.R')
+library(biomod2)
+# ##notebook
+# source('/home/anderson/R-Scripts/paleoextract.R')
+# source('/home/anderson/R-Scripts/strings2na.R')
+# source('/home/anderson/R-Scripts/dataInstance.R')
+# source('/home/anderson/R-Scripts/uplot.R')
+# ##source('/home/anderson/R-Scripts/uniche.R')
+# source('/home/anderson/R-Scripts/uniche2.R') #uniche2
+# source('/home/anderson/R-Scripts/cleanData.R')
+##Yavanna
+source('D:/Anderson_Eduardo/R/paleoextract.R')
+source('D:/Anderson_Eduardo/R/strings2na.R')
+source('D:/Anderson_Eduardo/R/dataInstance.R')
+source('D:/Anderson_Eduardo/R/uplot.R')
+source('D:/Anderson_Eduardo/R/uplot2.R')
 ##source('/home/anderson/R-Scripts/uniche.R')
-source('/home/anderson/R-Scripts/uniche2.R') #uniche2
-source('/home/anderson/R-Scripts/cleanData.R')
+source('D:/Anderson_Eduardo/R/uniche2.R') #uniche2
+source('D:/Anderson_Eduardo/R/uniche3.R') #uniche3
+source('D:/Anderson_Eduardo/R/cleanData.R')
 
 
 ##definindo parametros e variaveis globais##
-projectFolder = "/home/anderson/Projetos/SDM megafauna Sul-Americana" #pasta de trabalho do projeto
-envFolder = "/home/anderson/gridfiles/dados_projeto" #pasta das variaveis ambientais
-AmSulBorders = rgdal::readOGR('/home/anderson/shapefiles/Am_Sul/borders.shp') #shapefile da Am. do Sul
+# ##notebook
+# projectFolder = "/home/anderson/Projetos/SDM megafauna Sul-Americana" #pasta de trabalho do projeto
+# envFolder = "/home/anderson/gridfiles/dados_projeto" #pasta das variaveis ambientais
+# AmSulBorders = rgdal::readOGR('/home/anderson/shapefiles/Am_Sul/borders.shp') #shapefile da Am. do Sul
+##Yavanna
+projectFolder = "D:/Anderson_Eduardo/SDM megafauna Sul-Americana" #pasta de trabalho do projeto
+envFolder = "D:/Anderson_Eduardo/gridfiles/variaveis ambientais AmSul 120kyr" #pasta das variaveis ambientais
+AmSulBorders = rgdal::readOGR('D:/Anderson_Eduardo/shapefiles/Am_Sul/borders.shp') #shapefile da Am. do Sul
 
 
 
 ##abrindo e tratando o banco de dados##
 
 ##arquivo do banco de dados
-dataSetRaw = read.csv(file='/home/anderson/Projetos/SDM megafauna Sul-Americana/dataset_clean.csv', header=TRUE, dec='.', sep=',')
+#dataSetRaw = read.csv(file='/home/anderson/Projetos/SDM megafauna Sul-Americana/dataset_clean.csv', header=TRUE, dec='.', sep=',')
+dataSetRaw = read.csv(file='D:/Anderson_Eduardo/SDM megadauna Sul-Americana/dataset_clean.csv', header=TRUE, dec='.', sep=',')
+
 
 ##subset do banco de dados
 pts = dataSetRaw[,c('Species','Longitude','Latitude','Cal..Mean','Min.','Max.')]
@@ -34,7 +52,7 @@ pts = dataInstance(pts, c('Cal..Mean','Min.','Max.'), n=2*nrow(pts)) #criando in
 pts = cleanData(pts, c('Longitude','Latitude','age'), p=2) #arredondando coords, limpando NAs e dados duplicados
 pts = lapply( X=sample(x=seq(length(pts)), size=2*nrow(pts[[1]])), FUN=function(x) pts[[x]] ) #ajustando tamanho baseado no numero de pontos reais
 
-##inspe√ß√£o visual dos dados
+##inspeÁ„o visual dos dados
 plot(AmSulBorders)
 points(pts[[1]][,c('lon','lat')], pch=20, cex=1.5, col='red')
 
@@ -56,7 +74,9 @@ uplot(x=xx, shape=NULL, niche.metric='marginality')
 ##abrindo e tratando o banco de dados##
 
 ##arquivo do banco de dados
-dataSetRaw = read.csv(file='/home/anderson/Projetos/SDM megafauna Sul-Americana/dataset_clean.csv', header=TRUE, dec='.', sep=',')
+#dataSetRaw = read.csv(file='/home/anderson/Projetos/SDM megafauna Sul-Americana/dataset_clean.csv', header=TRUE, dec='.', sep=',')
+dataSetRaw = read.csv(file='D:/Anderson_Eduardo/SDM megadauna Sul-Americana/dataset_clean.csv', header=TRUE, dec='.', sep=',')
+
 
 ##subset do banco de dados
 pts = dataSetRaw[,c('Species','Longitude','Latitude','Cal..Mean','Min.','Max.')]
@@ -73,14 +93,50 @@ uplot(analise.incerteza, AmSulBorders, legend=FALSE) #inspecao visual - opcao 1
 
 
 
-#### rodando para todas as esp√©cies do banco de dados - somente um teste ainda ####
+
+#### rodando com o uniche3 ####
 
 
 
 ##abrindo e tratando o banco de dados##
 
 ##arquivo do banco de dados
-dataSetRaw = read.csv(file='/home/anderson/Projetos/SDM megafauna Sul-Americana/dataset_clean.csv', header=TRUE, dec='.', sep=',')
+#dataSetRaw = read.csv(file='/home/anderson/Projetos/SDM megafauna Sul-Americana/dataset_clean.csv', header=TRUE, dec='.', sep=',')
+dataSetRaw = read.csv(file='D:/Anderson_Eduardo/SDM megafauna Sul-Americana/dataset_clean.csv', header=TRUE, dec='.', sep=',')
+maxentFolder = 'D:/Anderson_Eduardo/maxent'
+
+##subset do banco de dados
+pts = dataSetRaw[,c('Species','Longitude','Latitude','Cal..Mean','Min.','Max.')]
+sp_i = 'Glyptodon clavipes' #'Notiomastodon platensis' #'Catonyx cuvieri'
+pts = pts[which(pts$Species == sp_i),]
+
+##ajustando dados
+pts = strings2na(pts, 'Species') #transformando strings ao longo do dateset em NA
+
+##analise de incerteza e sensibilidade##
+setwd(paste(projectFolder,'/Analise de sensibilidade e incerteza',sep=''))
+analise.incerteza = uniche3(pts, cols=c("Longitude","Latitude", "Cal..Mean","Min.","Max."), envFolder=envFolder, maxentFolder=maxentFolder) #analise de incerteza e sensibilidade - uniche3
+
+uplot2(analise.incerteza, AmSulBorders, legend=FALSE) #inspecao visual - opcao 1
+
+
+
+
+
+###################################################################################
+###################################################################################
+#### rodando para todas as espÈcies do banco de dados - somente um teste ainda ####
+###################################################################################
+###################################################################################
+
+
+
+##abrindo e tratando o banco de dados##
+
+##arquivo do banco de dados
+#dataSetRaw = read.csv(file='/home/anderson/Projetos/SDM megafauna Sul-Americana/dataset_clean.csv', header=TRUE, dec='.', sep=',')
+dataSetRaw = read.csv(file='D:/Anderson_Eduardo/SDM megafauna Sul-Americana/dataset_clean.csv', header=TRUE, dec='.', sep=',')
+maxentFolder = 'D:/Anderson_Eduardo/maxent'
 
 ##subset do banco de dados
 dataSetRaw = dataSetRaw[,c('Species','Longitude','Latitude','Cal..Mean','Min.','Max.')]
@@ -89,39 +145,37 @@ dataSetRaw = dataSetRaw[,c('Species','Longitude','Latitude','Cal..Mean','Min.','
 sps = as.character( unique(dataSetRaw$Species) )
 
 for (i in seq(length(sps))){
-
-    ##dataset da especie atual
-    pts = dataSetRaw[which(dataSetRaw$Species == sps[i]),]
-
-    if (nrow(pts) <= 5 | nrow(pts) > 50){
-        cat("\n ATEN√á√ÉO: A esp√©cie", sps[i], "possui menos que 5 registros, por isso n√£o foi analisada. \n")
-        next
-    }else{
-        cat("\n Rodando para a esp√©cie", sps[i], "...\n")
-    }
-
-    ##ajustando dados
-    pts = strings2na(pts, 'Species') #transformando strings ao longo do dateset em NA
-    pts = dataInstance(pts, c('Cal..Mean','Min.','Max.'), n=2*nrow(pts)) #criando instancias de dados (i.e. definindo idades a partir dos itervalos)
-    pts = cleanData(pts, c('Longitude','Latitude','age'), p=2) #arredondando coords, limpando NAs e dados duplicados
-
-    ## ##inspe√ß√£o visual dos dados
-    ## plot(AmSulBorders)
-    ## points(pts[[1]][,c('lon','lat')], pch=20, cex=1.5, col='red')
-
-    ##analise de incerteza e sensibilidade
-    analise.incerteza = uniche(x=pts, envFolder=envFolder) #analise de incerteza e sensibilidade
-
-    ##salvando output da analise
-    save(analise.incerteza, file=paste(projectFolder,'/Analise de sensibilidade e incerteza/',sps[i],'.R',sep=''))
-
-    ##salvando output grafico
-    jpeg(paste(projectFolder,'/Analise de sensibilidade e incerteza/',sps[i],'.jpeg',sep=''), 600, 600)
-    uplot(analise.incerteza, AmSulBorders, legend=FALSE)
-    dev.off()
-
-    gc()
-
+  
+  ##dataset da especie atual
+  pts = dataSetRaw[which(dataSetRaw$Species == sps[i]),]
+  
+  if (nrow(pts) <= 5){
+    cat("\n ATEN«√O: A espÈcie", sps[i], "possui menos que 5 registros, por isso n„o foi analisada. \n")
+    next
+  }else{
+    cat("\n Rodando para a espÈcie", sps[i], "...\n")
+  }
+  
+  ##ajustando dados
+  pts = strings2na(pts, 'Species') #transformando strings ao longo do dateset em NA
+  
+  ## ##inspeÁ„o visual dos dados
+  ## plot(AmSulBorders)
+  ## points(pts[[1]][,c('lon','lat')], pch=20, cex=1.5, col='red')
+  
+  ##analise de incerteza e sensibilidade
+  analise.incerteza = uniche3(x=pts, cols=c("Longitude","Latitude", "Cal..Mean","Min.","Max."), envFolder=envFolder, maxentFolder=maxentFolder) #analise de incerteza e sensibilidade
+  
+  ##salvando output da analise
+  save(analise.incerteza, file=paste(projectFolder,'/Analise de sensibilidade e incerteza/',sps[i],'.R',sep=''))
+  
+  ##salvando output grafico
+  jpeg(paste(projectFolder,'/Analise de sensibilidade e incerteza/',sps[i],'.jpeg',sep=''), 600, 600)
+  uplot(analise.incerteza, AmSulBorders, legend=FALSE)
+  dev.off()
+  
+  gc()
+  
 }
 
 
@@ -129,11 +183,6 @@ for (i in seq(length(sps))){
 
 
 ## for (i in seq(length(currentDataSet))){
-##     cat(" Rodando itera√ß√£o: ", i, "\n")
+##     cat(" Rodando iteraÁ„o: ", i, "\n")
 ##     teste = paleoextract(currentDataSet[[i]], cols=c('lon','lat','age'), path=envFolder)
 ## }
-
-
-
-
-
---
+++ uplot2.R
@@ -0,0 +1,97 @@
+##codigo para funcao uplot, que plota o resultado da analise de incerteza a partir do output da funcao 'uniche'.
+##Anderson A. Eduardo
+##12/Set/2018
+
+uplot2 = function(x, shape=NULL, niche.metric=NULL, legend=TRUE){
+  
+  if (class(x) != 'uniche'){
+    stop("Os dados de entrada (x) deve ser um objeto da classe 'uniche'.")
+  }
+  
+  if (is.null(shape) & is.null(niche.metric)){
+    stop("Quando um shapefile não é fornecido o argumento 'niche.metric' deve ser especificado (as opções são: 'TSS' e 'ROC').")
+  }
+  
+  if (length(niche.metric) > 1){
+    stop("Quando um shapefile não é fornecido o argumento 'niche.metric' deve ser especificado com uma das opções: 'marginility' e 'ROC'.")
+  }
+  
+  if (any(!niche.metric %in% c('TSS','ROC'))){
+    stop("O argumento de entrada 'niche.metric' deve ser 'TSS' ou 'ROC'.")
+  }
+  
+  
+  if( is.null(shape) ){
+    if (niche.metric == 'TSS'){
+      
+      sensitivity = rgb(abs(x$uniche.TSS$PRCC$original),1-abs(x$uniche.TSS$PRCC$original),0,0.9)
+      tempUncert = 2.5 + 2 * apply( x$uniche.TSS$X, 2, sd) / max(apply( x$uniche.TSS$X, 2, sd)) 
+      
+      points(x$dataset[,c('lon','lat')], col=sensitivity, cex=tempUncert, pch=20)
+      text(x$dataset[,c('lon','lat')], labels=x$dataset$id, cex= 0.7)
+      title('TSS')
+      
+      if (legend==TRUE){
+        legend('topright', legend=c('High','Intermediate','Low'), fill=rgb(c(1,0.5,0),c(0,0.5,1),0,0.9), border=NA, title='Sensitivity', bty='n')}
+      
+    }else{
+      
+      sensitivity = rgb(abs(x$uniche.ROC$PRCC$original),1-abs(x$uniche.ROC$PRCC$original),0,0.9)
+      tempUncert = 2.5 + 2 * apply( x$uniche.ROC$X, 2, sd) / max(apply( x$uniche.ROC$X, 2, sd)) 
+      
+      points(x$dataset[,c('lon','lat')], col=rgb(abs(x$uniche.ROC$PRCC$original),1-abs(x$uniche.ROC$PRCC$original),0,0.9), cex=1+3*abs(x$uniche.ROC$PRCC$original), pch=20)
+      text(x$dataset[,c('lon','lat')], labels=x$dataset$id, cex= 0.7)
+      
+      if (legend==TRUE){
+        legend('topright', legend=c('High','Intermediate','Low'), fill=rgb(c(1,0.5,0),c(0,0.5,1),0,0.9), border=NA, title='Sensitivity', bty='n')
+      }
+      title('ROC')
+    }
+    
+  }else{
+    
+    par(mfrow=c(2,2), las=2)
+    
+    ##
+    
+    tempUncert = 2.5 + 2 * apply( x$uniche.TSS$X, 2, sd) / max(apply( x$uniche.TSS$X, 2, sd)) 
+    sensitivity = rgb(abs(x$uniche.TSS$PRCC$original),1-abs(x$uniche.TSS$PRCC$original),0,0.9)
+    
+    ## plot(x$uniche.TSS)
+    ## points(xx$uniche.TSS$PRCC$original, pch=20, cex=tempUncert, col=sensitivity)
+    ## abline(h=0, col='red', lty=2)
+    
+    plot(seq(ncol(x$uniche.TSS$X)), apply( x$uniche.TSS$PRCC[,c('min. c.i.','max. c.i.')], 1, sum)/2, xaxt="n", xlab='Points', ylab='PRCC', main='TSS', ylim=c(-1,1), pch=20, cex=tempUncert-1.5, col=sensitivity)
+    arrows( x0=seq(ncol(x$uniche.TSS$X)), y0=x$uniche.TSS$PRCC$min, x1=seq(ncol(x$uniche.TSS$X)), y1=x$uniche.TSS$PRCC$max, code=0)
+    axis(1, at=seq(ncol(x$uniche.TSS$X)), labels=paste('Point ', x$dataset$id, sep=''), las=2)
+    abline(h=0, col='red', lty=2)
+    
+    plot(shape, main='TSS')
+    points(x$dataset[,c('lon','lat')], col=sensitivity, cex=tempUncert, pch=20)
+    text(x$dataset[,c('lon','lat')], labels=x$dataset$id, cex= 0.7)
+    
+    if (legend==TRUE){
+      legend('topright', legend=c('High sensitivity','Intermediate sensitivity','Low sensitivity', 'Temporal uncertainity'), fill=rgb(c(1,0.5,0,1),c(0,0.5,1,1),c(0,0,0,1),c(0.9,0.9,0.9,0)), pch=c(NA,NA,NA,1), pt.cex=2, border=NA, bty='n')
+    }
+    
+    ##
+    
+    tempUncert = 2.5 + 2 * apply( x$uniche.ROC$X, 2, sd) / max(apply( x$uniche.ROC$X, 2, sd)) 
+    sensitivity = rgb(abs(x$uniche.ROC$PRCC$original),1-abs(x$uniche.ROC$PRCC$original),0,0.9)
+    
+    plot(seq(ncol(x$uniche.ROC$X)), apply( x$uniche.ROC$PRCC[,c('min. c.i.','max. c.i.')], 1, sum)/2, xaxt="n", xlab='Points', ylab='PRCC', main='ROC', ylim=c(-1,1), pch=20, cex=tempUncert-1.5, col=sensitivity)
+    arrows( x0=seq(ncol(x$uniche.ROC$X)), y0=x$uniche.ROC$PRCC$min, x1=seq(ncol(x$uniche.ROC$X)), y1=x$uniche.ROC$PRCC$max, code=0)
+    axis(1, at=seq(ncol(x$uniche.ROC$X)), labels=paste('Point ', x$dataset$id, sep=''), las=2)
+    abline(h=0, col='red', lty=2)
+    
+    plot(shape, main='ROC')
+    points(x$dataset[,c('lon','lat')], col=sensitivity, cex=tempUncert, pch=20)
+    text(x$dataset[,c('lon','lat')], labels=x$dataset$id, cex= 0.7)
+    
+    if (legend==TRUE){
+      legend('topright', legend=c('High sensitivity','Intermediate sensitivity','Low sensitivity', 'Temporal uncertainity'), fill=rgb(c(1,0.5,0,1),c(0,0.5,1,1),c(0,0,0,1),c(0.9,0.9,0.9,0)), pch=c(NA,NA,NA,1), pt.cex=2, border=NA, bty='n')
+    }
+    
+    ##
+  }
+}
