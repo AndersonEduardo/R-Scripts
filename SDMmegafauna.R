02924f4c4b1902ede282297861c92e14ae4ba4b6
Anderson Eduardo <andersonaed@gmail.com>
2018-12-05 16:58:12 -0300

    avanço implementado SDMs

--- SDMmegafauna.R
+++ SDMmegafauna.R
@@ -32,6 +32,7 @@ source('D:/Anderson_Eduardo/R/cleanData.R')
 # AmSulBorders = rgdal::readOGR('/home/anderson/shapefiles/Am_Sul/borders.shp') #shapefile da Am. do Sul
 ##Yavanna
 projectFolder = "D:/Anderson_Eduardo/SDM megafauna Sul-Americana" #pasta de trabalho do projeto
+setwd(projectFolder)
 envFolder = "D:/Anderson_Eduardo/gridfiles/variaveis ambientais AmSul 120kyr" #pasta das variaveis ambientais
 AmSulBorders = rgdal::readOGR('D:/Anderson_Eduardo/shapefiles/Am_Sul/borders.shp') #shapefile da Am. do Sul
 
@@ -90,6 +91,10 @@ for (i in seq(length(sps))){
 
 ## SDM ##
 
+
+
+setwd(paste(projectFolder,'/SDMs',sep=''))
+
 ##pts choosed to be excluded
 ptsToExclude = list(
   Catonyx_chilensis = NA,
@@ -114,162 +119,145 @@ ptsToExclude = list(
 
 ##loop over species
 for (i in seq(length(sps))){
-
-  ##abrindo dados
-  load(paste(projectFolder,'/Analise de sensibilidade e incerteza/',sps[i],'.R',sep=''))
-
-  ##excluding pts
-  ptsToExcludeID = ptsToExclude[[agrep(sps[i], names(ptsToExclude))]]
-  if ( !is.na(ptsToExcludeID) ){
-    occData = analise.incerteza$dataset[ -match(ptsToExcludeID, analise.incerteza$dataset$ID),]
-  }else{
-    occData = analise.incerteza$dataset
-  }
+  tryCatch({
+    
+    ##sps folder
+    if (!file.exists(paste(projectFolder,'/SDMs/',sps[i],sep=''))){
+      dir.create(paste(projectFolder,'/SDMs/',sps[i],sep=''), recursive=TRUE)
+    }
+    setwd(paste(projectFolder,'/SDMs/',sps[i],sep=''))
   
-  ##cleaning the data
-  occData[,c('lon','lat')] = round(occData[,c('lon','lat')], 2)
-  occData = occData[!duplicated(occData[,c('lon','lat')]), ]
+    ##abrindo dados
+    load(paste(projectFolder,'/Analise de sensibilidade e incerteza/',sps[i],'.R',sep=''))
   
-  occDataList = dataInstance(x=occData, col_names=c('ageMean', 'ageMin', 'ageMax'), tempRes=1) #Obs.: put tempres=1 if age already is in the desired temporal resolution.
-  
-  iter=1 ##loop over data intances
-  for(j in seq(length(occDataList))){
-  
-    names(occDataList[[iter]]) = c("lon","lat","originalID","age")
-    
-    current_occData = paleoextract(x=occDataList[[iter]], path=envFolder)
-    current_occData = current_occData[complete.cases(current_occData),]
-    
-    ##cross-temporal background points
-    current_bgData = paleobg(x=current_occData, colNames=c('lon','lat','age'), envFolder=envFolder, n=10000) #sanpling bg points
-    
-    current_bgData[,c('x','y')] = round(current_bgData[,c('x','y')], 2) #round data
-    current_bgData = current_bgData[!duplicated(current_bgData[,c('x','y','age')]), ] #excluding duplicated points (in a same age)
-    current_bgData$originalID = NA #adjusting to fit occ data.frame
-    idxOrdem = match(names(current_occData), names(current_bgData)) #adjusting to fit occ data.frame
-    current_bgData = current_bgData[,idxOrdem] #adjusting to fit occ data.frame
-    
-    ##occ + bg points
-    current_occData$type = 'occ' #assigning data type
-    current_bgData$type = 'bg' #assigning data type
-    current_dataSet = rbind(current_occData, current_bgData) #dataset consolidation
-    current_dataSet = current_dataSet[complete.cases(current_dataSet[, grep(pattern='originalID', x=names(current_dataSet), invert=TRUE) ]), ]
-    
-    gc() #hoping to clean some computer memory...
-  
-    ##variaveis e parametros locais especificos para o biomod2
-    myRespName <- gsub(pattern=' ',x=sps[i], replacement='_') # nome do cenario atual (para biomod2)
-    myResp <- data.frame(type=as.integer(current_dataSet[,c('type')] == 'occ')) # variavel resposta (para biomod2)
-    myRespXY <- current_dataSet[,c('lon','lat')] # coordenadas associadas a variavel resposta (para biomod2)
-    myExpl = current_dataSet[,grep(pattern="originalID|age|type", x=names(current_dataSet),invert=TRUE)]  #variavel preditora (para biomod2)
-    
-    ##ajuste de dados de entrada para biomod2
-    myBiomodData <- BIOMOD_FormatingData(resp.var = myResp,
-                                         expl.var = myExpl,
-                                         resp.xy = myRespXY,
-                                         resp.name = myRespName)
-    
-    ##parametrizando os modelos
-    myBiomodOption <- BIOMOD_ModelingOptions(
-      MAXENT.Phillips=list(
-        path_to_maxent.jar=maxentFolder,
-        linear=TRUE,
-        quadratic=TRUE,
-        product=FALSE,
-        threshold=FALSE,
-        hinge=FALSE,
-        doclamp=FALSE,
-        maximumiterations=1000,
-        convergencethreshold=1.0E-5,
-        threads=4))
-    
-    ##rodando o(s) algoritmo(s) (i.e. SDMs)
-    myBiomodModelOut <- BIOMOD_Modeling(
-      myBiomodData,
-      models = c('MAXENT.Phillips'),
-      models.options = myBiomodOption,
-      NbRunEval = 100,
-      DataSplit = 75,
-      VarImport = 10,
-      models.eval.meth = c('TSS','ROC'),
-      SaveObj = FALSE,
-      rescal.all.models = TRUE,
-      do.full.models = FALSE,
-      modeling.id = paste(myRespName))
-    
-    ##My output data
-    evaluationScores = get_evaluations(myBiomodModelOut)
-  
-    ##salvando modelo no HD
-    save(myBiomodModelOut, file=paste(projectFolder,'/SDMs/',sps[i],'_SDM.R',sep=''))
-    
-    
-  
-    ##variableimportance final model
-    varImportOutput = rowMeans(SDMmaxent@results[grep("importance", rownames(SDMmaxent@results)), ])
-    varImportTable = data.frame(names(varImportOutput), permutation.importance=varImportOutput)
-    write.csv(varImportTable, paste(projectFolder,'/maxent/omobranchus_variablesImportance_finalModel.csv',sep=''), row.names=FALSE)
-    
-    ##predicao AREA NATIVA
-    names(predAreaNat) = gsub( pattern='^predAreaNat_', replacement='', x=names(predAreaNat) ) #retirando parte do nome para adequar ao modelo
-    predAreaNat = predAreaNat[[ grep(pattern=paste(varNames,collapse='|'), x=names(predAreaNat), value=TRUE) ]] #novo objeto, correto
-    SDMpredNativ = dismo::predict(SDMmaxent, predAreaNat, progress='text') #projecao espacial
-    crs(SDMpredNativ) = '+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0'
-    
-    ##predicao AREA INVADIDA
-    names(predAreaInv) = gsub( pattern='^predAreaInv_', replacement='', x=names(predAreaInv) ) #retirando parte do nome para adequar ao modelo
-    predAreaInv = predAreaInv[[ grep(pattern=paste(varNames,collapse='|'), x=names(predAreaNat), value=TRUE) ]] #novo objeto, correto
-    SDMpredInv = dismo::predict(SDMmaxent, predAreaInv, progress='text') #projecao espacial
-    crs(SDMpredInv) = '+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0'
-    
-    
-    ##salvando gridfiles
-    writeRaster(calc(SDMpredNativ,mean), paste(projectFolder,'/maxent/SDMpredNativ_Suitability_Mean.asc',sep=''), overwrite=TRUE) #area nativa
-    writeRaster(calc(SDMpredNativ,sd), paste(projectFolder,'/maxent/SDMpredNativ_Suitability_SD.asc',sep=''), overwrite=TRUE) #area nativa
-    ##
-    writeRaster(calc(SDMpredInv,mean),paste(projectFolder,'/maxent/SDMpredInv_Suitability_Mean.asc',sep=''), overwrite=TRUE) #area nativa
-    writeRaster(calc(SDMpredInv,sd),paste(projectFolder,'/maxent/SDMpredInv_Suitability_SD.asc',sep=''), overwrite=TRUE) #area nativa
-    
-    
-    ##calculo do threshold##
-    ##valores preditos pelo SDM em cada ponto do dataset na AREA NATIVA
-    pred =  extract(x=calc(SDMpredNativ,mean),
-                    y=dataSet[dataSet$area!='inv',c('lon','lat')],
-                    method='bilinear',
-                    na.rm=TRUE)
-    
-    threshDF = data.frame(occ=dataSet[dataSet$area!='inv',]$occ, pred=pred) #juntando predicoes e observacoes
-    threshDF = threshDF[complete.cases(threshDF),] #retirando possiveis NAs
-    
-    ##OBS.: omissao -> falso negativo -> false negative rate (FNR) = 1 - TPR (True Positive Rate)
-    ##TPR = sensitividade
-    myRoc = roc(predictor=threshDF$pred, response=threshDF$occ, positive=1) #curva ROC
-    rocDF = data.frame( FNR = 1-myRoc$sensitivities, thresholds = myRoc$thresholds ) #data.frame com omissao e thresholds
-    thre = max(rocDF[which(rocDF$FNR == max(rocDF[rocDF$FNR <= 0.05 ,]$FNR)),]$thresholds) #thresold de 5% omissao
-    
-    
-    ##calculo do TSS para com o threshold -- OBS: TSS = sensitivity + specificity - 1
-    TSSvalue = myRoc$sensitivities[which(myRoc$thresholds == thre)] + myRoc$specificities[which(myRoc$thresholds == thre)] - 1
-    
-    
-    ##salvando output do ENMeval
-    SDMevalOutput = as.data.frame(SDMeval@results)
-    write.csv(SDMevalOutput, paste(projectFolder,'/maxent/omobranchus_SDMeval.csv',sep=''), row.names=FALSE)
-    
-    
-    ##salvando graficos do ENMeval
-    jpeg(paste(projectFolder,'/maxent/omobranchus_SDMeval.jpg',sep=''), width=800)
-    par(mfrow=c(1,2), mar=c(5,5,10,5))
-    eval.plot(SDMeval@results)
-    eval.plot(SDMeval@results, 'Mean.AUC', var='Var.AUC')
-    dev.off()
-    
-    
-    ##salvando imagens jpeg no HD
-  
+    ##excluding pts
+    ptsToExcludeID = ptsToExclude[[agrep(sps[i], names(ptsToExclude))]]
+    if ( !is.na(ptsToExcludeID) ){
+      occData = analise.incerteza$dataset[ -match(ptsToExcludeID, analise.incerteza$dataset$ID),]
+    }else{
+      occData = analise.incerteza$dataset
     }
     
-  }
+    ##cleaning the data
+    occData[,c('lon','lat')] = round(occData[,c('lon','lat')], 2)
+    occData = occData[!duplicated(occData[,c('lon','lat')]), ]
+    
+    occDataList = dataInstance(x=occData, col_names=c('ageMean', 'ageMin', 'ageMax'), tempRes=1) #Obs.: put tempres=1 if age already is in the desired temporal resolution.
+    
+    ##loop over data intances
+    for(j in seq(length(occDataList))){ 
+      
+      gc() #hoping to clean some computer memory...
+      
+      names(occDataList[[iter]]) = c("lon","lat","originalID","age")
+      
+      current_occData = paleoextract(x=occDataList[[iter]], path=envFolder)
+      current_occData = current_occData[complete.cases(current_occData),]
+      
+      ##cross-temporal background points
+      current_bgData = paleobg(x=current_occData, colNames=c('lon','lat','age'), envFolder=envFolder, n=10000) #sanpling bg points
+      
+      current_bgData[,c('x','y')] = round(current_bgData[,c('x','y')], 2) #round data
+      current_bgData = current_bgData[!duplicated(current_bgData[,c('x','y','age')]), ] #excluding duplicated points (in a same age)
+      current_bgData$originalID = NA #adjusting to fit occ data.frame
+      idxOrdem = match(names(current_occData), names(current_bgData)) #adjusting to fit occ data.frame
+      current_bgData = current_bgData[,idxOrdem] #adjusting to fit occ data.frame
+      
+      ##occ + bg points
+      current_occData$type = 'occ' #assigning data type
+      current_bgData$type = 'bg' #assigning data type
+      current_dataSet = rbind(current_occData, current_bgData) #dataset consolidation
+      current_dataSet = current_dataSet[complete.cases(current_dataSet[, grep(pattern='originalID', x=names(current_dataSet), invert=TRUE) ]), ]
+      
+      ##variaveis e parametros locais especificos para o biomod2
+      myRespName <- paste( gsub(pattern=' ',x=sps[i], replacement='_'), '_instance_',j, sep='') # nome do cenario atual (para biomod2)
+      myResp <- data.frame(type=as.integer(current_dataSet[,c('type')] == 'occ')) # variavel resposta (para biomod2)
+      myRespXY <- current_dataSet[,c('lon','lat')] # coordenadas associadas a variavel resposta (para biomod2)
+      myExpl = current_dataSet[,grep(pattern="lon|lat|originalID|age|type", x=names(current_dataSet),invert=TRUE)]  #variavel preditora (para biomod2)
+      
+      ##ajuste de dados de entrada para biomod2
+      myBiomodData <- BIOMOD_FormatingData(resp.var = myResp,
+                                           expl.var = myExpl,
+                                           resp.xy = myRespXY,
+                                           resp.name = myRespName)
+      
+      ##parametrizando os modelos
+      myBiomodOption <- BIOMOD_ModelingOptions(
+        MAXENT.Phillips=list(
+          path_to_maxent.jar=maxentFolder,
+          linear=TRUE,
+          quadratic=TRUE,
+          product=FALSE,
+          threshold=FALSE,
+          hinge=FALSE,
+          doclamp=FALSE,
+          maximumiterations=1000,
+          convergencethreshold=1.0E-5,
+          threads=4))
+      
+      ##rodando o(s) algoritmo(s) (i.e. SDMs)
+      myBiomodModelOut <- BIOMOD_Modeling(
+        myBiomodData,
+        models = c('MAXENT.Phillips'),
+        models.options = myBiomodOption,
+        NbRunEval = 100,
+        DataSplit = 75,
+        VarImport = 10,
+        models.eval.meth = c('TSS','ROC'),
+        SaveObj = FALSE,
+        rescal.all.models = TRUE,
+        do.full.models = FALSE,
+        modeling.id = myRespName)
+      
+      ##My output data
+      evaluationScores = get_evaluations(myBiomodModelOut)
+    
+      ##salvando modelo no HD
+      save(myBiomodModelOut, file=paste(projectFolder,'/SDMs/',gsub(pattern=' ',x=sps[i], replacement='_'),'_instance_',j,'_SDM','.R',sep=''))
+      
+      ##variableimportance final model
+      varImport = get_variables_importance(myBiomodModelOut)
+      varImportMean = rowMeans(varImport[,,1:100,], na.rm=TRUE)
+      varImportSD = apply(varImport[,,1:100,],1, sd )
+      varImportDF = data.frame(variables=names(varImportMean), importance.mean=varImportMean, impotance.sd=varImportSD)
+      write.csv(varImportDF, paste(projectFolder,'/SDMs/',sps[i],'/',gsub(pattern=' ',replacement='_',x=sps[i]),'_VariableImportance.csv',sep=''), row.names=FALSE)
+      
+      ##predicao
+      envVarPaths = list.files(envFolder, full.names=TRUE)
+      
+      for (l in 1:length(envVarPaths[1:24])){
+        
+        ##definindo variaveis e parametros internos
+        predictors = stack(list.files(path=envVarPaths[l], full.names=TRUE, pattern='.asc')) #predictors com todas as variaveis (presente)
+        ##predictors = predictors[[c('bioclim_01','bioclim_12')]]
+        ##predictors = mask(predictors,AmSulShape) #recortando as variaveis ambientais
+        crs(predictors) = CRS('+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0') #ajustando CRS
+        
+        myBiomodModelFull <- BIOMOD_Modeling(
+          data = myBiomodData,
+          models = c('MAXENT.Phillips'),
+          models.options = myBiomodOption,
+          NbRunEval = 1,
+          DataSplit = 100,
+          SaveObj = FALSE,
+          rescal.all.models = TRUE,
+          do.full.models = TRUE,
+          modeling.id = paste(myRespName,'_FULL',sep=''))
+        
+        ##rodando algortmo de projecao (i.e. rodando a projecao)
+        myBiomodProj <- BIOMOD_Projection(
+          modeling.output = myBiomodModelFull,
+          new.env = predictors,
+          proj.name = paste(l-1,'kyr',sep=''),
+          selected.models = 'all',
+          compress = 'TRUE',
+          build.clamping.mask = 'TRUE',
+          output.format = '.grd')
+        
+      }
+    }
+  }, error=function(e){cat("ERROR :",conditionMessage(e), "\n")})
+}
 
 
 
